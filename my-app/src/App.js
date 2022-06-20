6159d556d0fb8571ab4cbe3aae710ea284458abe
Monika Jakobsson <nimrin81@gmail.com>
2022-06-20 23:35:07 +0000

    Add event handling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreeting from './components/FunctionalGreetings';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingsWithProps';
-import StatefulGreeting from './components/StatefulGreeting';
-import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
-import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import EventsClass from './components/EventsClass';
+import EventsFunctional from './components/EventsFunctional';
+// import FunctionalGreeting from './components/FunctionalGreetings';
+// import FunctionalGreetingWithProps from './components/FunctionalGreetingsWithProps';
+// import StatefulGreeting from './components/StatefulGreeting';
+// import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+// import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting />
+      {/* <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting="Nice to meet you!" name="John" age="32" />
       <StatefulGreeting greeting="I'm a stateful component" name="Tim"/>
       <StatefulGreetingWithCallback />
-      <StatefulGreetingWithPrevState />
+      <StatefulGreetingWithPrevState /> */}
+      <EventsClass />
+      <EventsFunctional />
     </div>
   );
 }
