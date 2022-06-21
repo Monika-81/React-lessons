8ff7526ff49ca78a5c63707159bbe0abba087c81
Monika Jakobsson <nimrin81@gmail.com>
2022-06-21 23:21:53 +0000

    submitting form data

--- my-app/src/components/ControlledForm.js
+++ my-app/src/components/ControlledForm.js
@@ -5,28 +5,79 @@ export class ControlledForm extends Component {
     super(props)
     
     this.state = {
-      name: ''
+      name: '',
+      category: 'website',
+      comments:''
     }
   }
 
-  handleNameChange = (event) => {
-    this.setState({
-      name: event.target.value
-    })
-  }
+  // handleNameChange = (event) => {
+  //   this.setState({
+  //     name: event.target.value,
+  //   })
+  // }
+
+  // handleCategoryChange = (event) => {
+  //   this.setState({
+  //     category: event.target.value,
+  //   })
+  // }
+
+  // handleCommentsChange = (event) => {
+  //   this.setState({
+  //     comments: event.target.value,
+  //   })
+  // }
+
+    handleChange = (event) => {
+      const {name, value} = event.target
+      this.setState({
+        [name]: value
+      })
+    }
+
+    handleSubmit = (event) => {
+      event.preventDefault();
+      console.log(this.state)
+    }
 
   render() {
     return (
       <div>
-        <form>
-          <label htmlFor='id-name'>Your Name:</label>
-          <input 
-            value={this.state.name} 
-            id="id-name" 
-            name="name" 
-            type="text" 
-            onChange={this.handleNameChange}
-          />
+        <h1>Please fill out the form below:</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor='id-name'>Your Name:</label>
+            <input 
+              value={this.state.name} 
+              id="id-name" 
+              name="name" 
+              type="text" 
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='id-category'>Query category:</label>
+            <select 
+              id="id-category" 
+              name="category"
+              value={this.state.category}
+              onChange={this.handleChange}
+              >
+              <option value="website">Website issue</option>
+              <option value="order">Order issue</option>
+              <option value="general">General inquiry</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="id-comments">Comments:</label>
+            <textarea 
+              id="id-comments" 
+              name='comments' 
+              value={this.state.comments} 
+              onChange={this.handleChange}
+            />
+          </div>
           <input type="submit" value="Submit" />
         </form>
       </div>
